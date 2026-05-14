b16962a7e712461eb037f3a63b554fb3e551d28b
Gurupreet Singh Sachdev <gurupreetsinghsachdev@Gurupreets-MacBook-Air.local>
2026-05-14 14:48:00 +0530

    gemini model change

--- lib/ai.action.ts
+++ lib/ai.action.ts
@@ -30,7 +30,7 @@ export const generate3DView = async ({ sourceImage }: Generate3DViewParams) => {
 
     const response = await puter.ai.txt2img(VaastuX_RENDER_PROMPT, {
         provider: "gemini",
-        model: "gemini-2.5-flash-image-preview",
+        model: "google/gemini-3.1-flash-image-preview",
         input_image: base64Data,
         input_image_mime_type: mimeType,
         ratio: { w: 1024, h: 1024 },
